f653f29261be0b1ff801c66751508165d58b0fae
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-09-26 14:46:48 +0200

    Made various changes :
    - Defined Inter Google Font as default
    - Created Header component
    - Created Footer component
    - Added banner image
    - Completed Home Page

---
+++ HRnet-React/src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header(): JSX.Element {
+  return (
+    <header className="flex h-24 items-center justify-between px-16 py-4 text-neutral-900">
+      <Link
+        className="flex items-center justify-between gap-4 transition duration-150 hover:text-blue-500"
+        to="/"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+          <polyline points="9 22 9 12 15 12 15 22"></polyline>
+        </svg>
+        <h1 className="text-2xl font-bold">HRNet</h1>
+      </Link>
+
+      <nav className="flex justify-between gap-8 text-xl">
+        <NavLink
+          className="transition duration-150 hover:text-blue-500 hover:underline"
+          to="/employee/list"
+        >
+          Employee list
+        </NavLink>
+
+        <NavLink
+          className="transition duration-150 hover:text-blue-500 hover:underline"
+          to="/employee/create"
+        >
+          Add employee
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
--- HRnet-React/src/pages/Home.tsx
+++ HRnet-React/src/pages/Home.tsx
@@ -1,12 +1,28 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import bannerImage from "../assets/banner.jpg";
+
 /**
  * Renders the Home component.
  *
  * @return {JSX.Element} The rendered Home component.
  */
 export default function Home(): JSX.Element {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <section className="flex min-h-[calc(100vh-12rem)] flex-col items-center justify-center py-16 text-neutral-900">
+        <img
+          className="h-full object-cover"
+          src={bannerImage}
+          alt="Hero Image"
+        />
+        <h2 className="text-center text-8xl font-bold">Welcome to HRNet</h2>
+        <p className="mt-8 text-center text-3xl">
+          Your one-stop solution for managing employees.
+        </p>
+      </section>
+      <Footer />
+    </div>
+  );
+}
