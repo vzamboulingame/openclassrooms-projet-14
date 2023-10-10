aee53bc3c22379751c7f9bdb1a62e9aeefbe06c7
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-10-10 17:48:49 +0200

    Made various changes :
    - Added "@mui/x-data-grid" package
    - Imported DataGrid component in EmployeeList.tsx
    - Created table with sortable columns
    - Added default pageSize of 5
    - Added following pageSizeOptions : 5, 10, 25

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useEmployeeStore } from "../utils/store";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -7,11 +10,85 @@ import Footer from "../components/Footer";
  * @return {JSX.Element} The rendered EmployeeList component.
  */
 export default function EmployeeList(): JSX.Element {
+  const { employeeDataArray } = useEmployeeStore();
+
+  const [paginationModel, setPaginationModel] = useState({
+    pageSize: 5,
+    page: 0,
+  });
+
+  // Define the columns based on EmployeeData interface
+  const columns: GridColDef[] = [
+    {
+      field: "firstName",
+      headerName: "First Name",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "dateOfBirth",
+      headerName: "Date of Birth",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "street",
+      headerName: "Street",
+      sortable: true,
+      width: 200,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "state",
+      headerName: "State",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "department",
+      headerName: "Department",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "startDate",
+      headerName: "Start Date",
+      sortable: true,
+      width: 150,
+    },
+  ];
+
   return (
     <div>
       <Header />
       <section className="flex min-h-[calc(100vh-12rem)] flex-col items-center text-neutral-900 ">
-        <h1>EmployeeList</h1>
+        <h1 className="mb-8 text-center text-2xl">Current Employees</h1>
+        <div className="flex flex-col">
+          <DataGrid
+            rows={employeeDataArray}
+            columns={columns}
+            paginationModel={paginationModel}
+            onPaginationModelChange={setPaginationModel}
+            pageSizeOptions={[5, 10, 25]}
+          />
+        </div>
       </section>
       <Footer />
     </div>
