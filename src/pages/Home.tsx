134bc4d16f3179b2af7ffaa2f34caf62fe6ced76
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-10-12 20:06:39 +0200

    Added tablet & mobile versions of Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,14 +11,16 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Header />
-      <section className="flex min-h-[calc(100vh-12rem)] flex-col items-center text-neutral-900">
+      <section className="flex min-h-[calc(100vh-12rem)] flex-col items-center justify-center gap-y-16 py-4 text-neutral-900 sm:gap-y-6 sm:py-2 lg:gap-y-10">
         <img
-          className="h-full max-h-[60vh] object-scale-down"
+          className="h-full max-h-[50vh] object-scale-down"
           src={bannerImage}
           alt="Banner Image"
         />
-        <h2 className="m-8 text-center text-7xl font-bold">Welcome to HRNet</h2>
-        <p className="mt-8 text-center text-4xl">
+        <h2 className="text-center text-7xl font-bold sm:mt-2 sm:text-3xl lg:text-5xl">
+          Welcome to HRNet
+        </h2>
+        <p className="text-center text-4xl sm:mt-1 sm:text-xl lg:text-2xl">
           Your one-stop solution for managing employees.
         </p>
       </section>
