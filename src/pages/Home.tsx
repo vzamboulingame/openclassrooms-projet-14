86f8a5844dd57210e4360c6639e244990b54a1af
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-10-16 12:58:15 +0200

    Added responsive images to Home & NotFound pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import bannerImage from "../assets/banner.jpg";
+import bannerJpg from "../assets/banner.jpg";
+import bannerWebp from "../assets/banner.webp";
+import bannerSmallWebp from "../assets/banner-small.webp";
 
 /**
  * Creates and renders the Home component.
@@ -12,15 +14,25 @@ export default function Home(): JSX.Element {
     <div>
       <Header />
       <section className="flex min-h-[calc(100vh-12rem)] flex-col items-center justify-center gap-y-12 text-neutral-900 xl:gap-y-10 md:gap-y-8 sm:gap-y-4">
-        <img
-          className="h-full max-h-[60vh] w-full object-scale-down"
-          src={bannerImage}
-          alt="Banner Image"
-          loading="lazy"
-        />
+        <picture>
+          <source
+            srcSet={bannerSmallWebp}
+            type="image/webp"
+            media="(max-width: 768px)"
+          />
+          <source srcSet={bannerWebp} type="image/webp" />
+          <img
+            className="h-full max-h-[60vh] w-full object-scale-down"
+            src={bannerJpg}
+            alt="Banner Image"
+            fetchPriority="high"
+          />
+        </picture>
+
         <h1 className="text-center text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl">
           Welcome to HRNet
         </h1>
+
         <p className="text-center text-4xl lg:text-3xl md:text-2xl sm:text-xl">
           Your one-stop solution for managing employees.
         </p>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import errorImg from "../assets/error.jpg";
+import errorJpg from "../assets/error.jpg";
+import errorWebp from "../assets/error.webp";
+import errorSmallWebp from "../assets/error-small.webp";
 
 /**
  * Creates and renders the NotFound component.
@@ -13,15 +15,25 @@ export default function NotFound(): JSX.Element {
     <div>
       <Header />
       <section className="flex min-h-[calc(100vh-12rem)] flex-col items-center justify-center gap-y-12 text-neutral-900 xl:gap-y-10 md:gap-y-8 sm:gap-y-4">
-        <img
-          className="h-full max-h-[60vh] w-full object-scale-down"
-          src={errorImg}
-          alt="404 Error Image"
-          loading="lazy"
-        />
+        <picture>
+          <source
+            srcSet={errorSmallWebp}
+            type="image/webp"
+            media="(max-width: 768px)"
+          />
+          <source srcSet={errorWebp} type="image/webp" />
+          <img
+            className="h-full max-h-[60vh] w-full object-scale-down"
+            src={errorJpg}
+            alt="404 Error Image"
+            fetchPriority="high"
+          />
+        </picture>
+
         <h1 className="text-center text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl">
           Page not found !
         </h1>
+
         <Link
           className="text-4xl transition duration-150 hover:text-blue-500 hover:underline lg:text-3xl md:text-2xl sm:text-xl"
           to="/"
