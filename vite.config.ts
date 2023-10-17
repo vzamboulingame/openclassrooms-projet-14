e5d21425a40ef8c34e8274dd9e37b9e606f9552a
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-10-17 15:31:41 +0200

    Added vite-plugin-checker for TS type checking

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
   build: {
     chunkSizeWarningLimit: 1500,
   },
